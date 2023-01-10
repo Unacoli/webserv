2162fc30b55017c718ab313adaca01d7f41e22ac
Bastien RODRIGUEZ <barodrig@e2r6p8.clusters.42paris.fr>
2023-01-10 10:58:58 +0100

    Corrected Config.cpp - functions were coded in c++11

--- srcs/config/Config.cpp
+++ srcs/config/Config.cpp
@@ -6,7 +6,7 @@
 /*   By: barodrig <barodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 20:29:00 by barodrig          #+#    #+#             */
-/*   Updated: 2023/01/09 21:20:12 by barodrig         ###   ########.fr       */
+/*   Updated: 2023/01/10 10:54:07 by barodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,38 +47,43 @@ Config &				Config::operator=( Config const & rhs )
 
 std::ostream &                operator<<( std::ostream & o, Config const & i )
 {
-    for (t_server server: i._server)
+    for ( std::vector<t_server>::const_iterator server = i._server.begin();
+            server != i._server.end(); server++ )
     {
-        o << "- - - - - - - SERVER CONFIGURATION " << server.names[0] << " - - - - - - -" << std::endl\
-            << "- Port = " << server.listen.port << std::endl\
-            << "- Host = " << server.listen.host << std::endl\
-            << "- Server Name(s) = ";
-            for (std::string str: server.names)
-                o << str << ' ';
-            o << std::endl << "- Server root = " << server.root << std::endl\
+        o << "- - - - - - - SERVER CONFIGURATION " << server->names[0] << " - - - - - - -" << std::endl\
+            << "- Port = " << server->listen.port << std::endl\
+            << "- Host = " << server->listen.host << std::endl\
+            << "- Server Name(s) = ";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+            for ( std::vector<std::string>::const_iterator str = server->names.begin();
+                    str != server->names.end(); str++ )
+                o << *str << ' ';
+            o << std::endl << "- Server root = " << server->root << std::endl\
             << "- Error codes : " << std::endl;
-            for (std::map<size_t, std::string>::const_iterator it = server.errors.begin();
-                    it != server.errors.end(); it++)
+            for (std::map<size_t, std::string>::const_iterator it = server->errors.begin();
+                    it != server->errors.end(); it++)
                 o << "Code = " << it->first << " > URI = " << it->second << std::endl; 
-            for (t_location location: server.locations)
+            for ( std::vector<t_location>::const_iterator location = server->locations.begin();
+                    location != server->locations.end(); location++ )
             {
-                o << "- - - LOCATION " << location.name << " - - -" << std::endl\
-                << "- Location Name = " << location.name << std::endl\
-                << "- Location Root = " << location.root << std::endl\
+                o << "- - - LOCATION " << location->name << " - - -" << std::endl\
+                << "- Location Name = " << location->name << std::endl\
+                << "- Location Root = " << location->root << std::endl\
                 << "- Location index = ";
-                for(std::string index: location.index)
-                    o << index << " ";
+                for ( std::vector<std::string>::const_iterator index = location->index.begin();
+                        index != location->index.end(); index++ )
+                    o << *index << " ";
                 o << std::endl << "- Location HTTP Methods = ";
-                for(std::string method: location.methods)
-                    o << method << " ";
-                o << std::endl << "- Client Body Size = " << location.client_body_size << std::endl\
-                << "- Upload Path = " << location.upload_path << std::endl\
-                << "- Upload Status = " << location.upload_status << std::endl\
-                << "- Autodindex = " << location.autoindex << std::endl\
-                << "- CGI Param = " << location.cgiparam << std::endl\
+                for ( std::vector<std::string>::const_iterator method = location->methods.begin();
+                        method != location->methods.end(); method++ )
+                    o << *method << " ";
+                o << std::endl << "- Client Body Size = " << location->client_body_size << std::endl\
+                << "- Upload Path = " << location->upload_path << std::endl\
+                << "- Upload Status = " << location->upload_status << std::endl\
+                << "- Autodindex = " << location->autoindex << std::endl\
+                << "- CGI Param = " << location->cgiparam << std::endl\
                 << "- CGI Pass = " << std::endl;
-                for (std::map<std::string, std::string>::const_iterator it = location.cgipass.begin();
-                        it != location.cgipass.end(); it++)
+                for (std::map<std::string, std::string>::const_iterator it = location->cgipass.begin();
+                        it != location->cgipass.end(); it++)
                     o << it->first << ":" << it->second << std::endl;
                 o << std::endl;
             }
