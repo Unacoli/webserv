b4862d86539c2af06a785ff1f4065e922f12fe1b
Bastien RODRIGUEZ <barodrig@e2r3p7.42.fr>
2023-01-16 15:14:30 +0100

    Cleaned junk files

--- srcs/main.d
+++
@@ -1,5 +0,0 @@
-srcs/main.o: srcs/main.cpp srcs/include/main.hpp \
-  srcs/config/include/Parsing.hpp srcs/include/Utils.hpp \
-  srcs/socket/include/WebServer.hpp \
-  srcs/config/include/ConfigDataStruct.hpp \
-  srcs/config/include/Config.hpp srcs/socket/include/server.hpp
