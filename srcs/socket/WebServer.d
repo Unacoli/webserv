f16197e9449e854507a6d7947c0e4aa64f5f718d
Bastien RODRIGUEZ <barodrig@e1r3p3.clusters.42paris.fr>
2023-02-09 14:36:48 +0100

    Cleaned

--- srcs/socket/WebServer.d
+++
@@ -1,6 +0,0 @@
-srcs/socket/WebServer.o: srcs/socket/WebServer.cpp \
-  srcs/socket/include/WebServer.hpp \
-  srcs/config/include/ConfigDataStruct.hpp \
-  srcs/config/include/Config.hpp srcs/config/include/Parsing.hpp \
-  srcs/include/main.hpp srcs/include/Utils.hpp \
-  srcs/socket/include/server.hpp
--- srcs/socket/client.d
+++
@@ -1,1 +0,0 @@
-srcs/socket/client.o: srcs/socket/client.cpp
