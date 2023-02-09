c28e436b08258406239f706ac71d4c61e1d71141
clodaghsmurphy <clodagh9@yahoo.ie>
2023-02-09 10:05:42 +0100

    pulll bastien

--- srcs/handle_responses.cpp
+++ srcs/handle_responses.cpp
@@ -102,7 +102,7 @@ std::map<std::string, t_server> > server_list, std::map<int, Client> &clients)
 	{
 		clients[client_fd]._response = new ResponseHTTP(*request, server);
 		turn_on_epollout(current_event, epfd, i);
-        send_response(current_event, clients, i, epfd);
+        send_response(client_fd, current_event, clients, i, epfd);
 
 	}
 
