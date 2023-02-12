986fd6a7085ce48ae830121d0dc7306832210d7b
Nao ARGOUSE <nargouse@e1r2p14.clusters.42paris.fr>
2023-02-12 17:02:42 +0100

    leaks

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -3,15 +3,16 @@
 Client::Client() 
 {
     _request = new RequestHTTP;
-    _response = new ResponseHTTP;
+//    _response = new ResponseHTTP;
     response_created = 0;
     resp_pos = 0;
 }
 
 Client::~Client()
 {
-    //delete (this->_request);
-    //delete (this->_response);
+    delete (this->_request);
+    if (response_created == 1)
+        delete (this->_response);
     return ;
 }
 
